252b76a275c272f7a192072f3ba22e9071c0565e
Aleksandar <acile067@gmail.com>
2025-06-29 16:28:44 +0200

    init

---
+++ terraform/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0"
+    }
+  }
+
+  backend "s3" {
+
+    region  = "eu-central-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
