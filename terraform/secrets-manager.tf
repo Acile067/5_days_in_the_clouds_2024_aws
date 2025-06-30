aca3bceb7c038c07a814ea3137c58857c1189c83
Aleksandar <acile067@gmail.com>
2025-06-30 17:11:34 +0200

    debug

--- terraform/secrets-manager.tf
+++ terraform/secrets-manager.tf
@@ -1,8 +1,8 @@
-resource aws_secretsmanager_secret main {
+/*resource aws_secretsmanager_secret main {
   name = "main-ssh-private-key"
 }
 
 resource aws_secretsmanager_secret_version main {
   secret_id     = aws_secretsmanager_secret.main.id
   secret_string = tls_private_key.main.private_key_pem
-}
+}*/
