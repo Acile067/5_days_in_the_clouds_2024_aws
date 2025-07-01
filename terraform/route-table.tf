151dd6d4f510d803b04394bc3d05f0b7c526077c
Aleksandar <acile067@gmail.com>
2025-07-01 17:26:13 +0200

    rta3

--- terraform/route-table.tf
+++ terraform/route-table.tf
@@ -16,3 +16,7 @@ resource aws_route_table_association rta2 {
   subnet_id      = aws_subnet.database.id
   route_table_id = aws_route_table.main.id
 }
+resource aws_route_table_association rta3 {
+  subnet_id      = aws_subnet.database_az2.id
+  route_table_id = aws_route_table.main.id
+}
