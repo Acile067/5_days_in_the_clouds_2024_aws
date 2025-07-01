2dc22e63f451c902462158c425406be38b153878
Aleksandar <acile067@gmail.com>
2025-07-01 20:45:48 +0200

    appgtw v1.0

--- terraform/application-gateway.tf
+++ terraform/application-gateway.tf
@@ -1,4 +1,4 @@
-/*resource aws_lb main {
+resource aws_lb main {
   name               = "main_application_gateway"
   internal           = false
   load_balancer_type = "application"
@@ -34,4 +34,4 @@ resource aws_lb_listener listener {
     target_group_arn = aws_lb_target_group.main.arn
     type             = "forward"
   }
-}*/
+}
