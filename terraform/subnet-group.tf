3411e3002dfdebf55cc589a367e355fb57013fed
Aleksandar <acile067@gmail.com>
2025-07-01 17:25:21 +0200

    small fix

--- terraform/subnet-group.tf
+++ terraform/subnet-group.tf
@@ -1,4 +1,7 @@
 resource aws_db_subnet_group mssql_subnet_group {
   name       = "mssql-subnet-group"
-  subnet_ids = [aws_subnet.backend.id, aws_subnet.database.id]
+  subnet_ids = [
+    aws_subnet.database.id,
+    aws_subnet.database_az2.id
+  ]
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -15,3 +15,10 @@ resource aws_subnet database {
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 }
+
+resource "aws_subnet" "database_az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+}
