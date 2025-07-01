a2662b9f681816e75049d649d3401d5295764bd3
Aleksandar <acile067@gmail.com>
2025-07-01 17:58:56 +0200

    con string change

--- terraform/application-gateway.tf
+++ terraform/application-gateway.tf
@@ -9,20 +9,20 @@
 
 resource aws_lb_target_group main {
   name     = "main_target_group"
-  port     = 80
+  port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
   health_check {
     path = "/test"
-    port = "traffic-port"   
+    port = 5000   
   }
 }
 
 resource aws_lb_target_group_attachment backend_attach {
   target_group_arn = aws_lb_target_group.main.arn
   target_id        = aws_instance.backend.id
-  port             = 80
+  port             = 5000
 }
 
 resource aws_lb_listener listener {
