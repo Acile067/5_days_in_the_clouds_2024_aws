90e897538b955492ce1fa96232cbaa2029210fd4
Aleksandar <acile067@gmail.com>
2025-07-01 21:27:12 +0200

    api gateway v2.1

--- terraform/application-gateway.tf
+++ terraform/application-gateway.tf
@@ -12,7 +12,7 @@ resource aws_apigatewayv2_stage main {
 resource "aws_apigatewayv2_integration" "main" {
   api_id          = aws_apigatewayv2_api.main.id
   integration_type = "http_proxy"
-  integration_uri  = "http://${aws_instance.main.public_ip}:5000/{proxy}"
+  integration_uri  = "http://${aws_instance.backend.public_ip}:5000/{proxy}"
   integration_method = "ANY"
   connection_type = "INTERNET"
 }
