e40a2b41bedb56087f6c4f42eb930f6612cf2b0e
Aleksandar <acile067@gmail.com>
2025-07-02 14:38:42 +0200

    sudo

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -6,15 +6,15 @@ dnf update -y
 dnf install -y wget unzip git jq zip
 
 # Preuzimanje .NET 9 runtime
-wget https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.6/aspnetcore-runtime-9.0.6-linux-x64.tar.gz
+sudo wget https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.6/aspnetcore-runtime-9.0.6-linux-x64.tar.gz
 
-mkdir -p $HOME/dotnet && tar zxf aspnetcore-runtime-9.0.6-linux-x64.tar.gz -C $HOME/dotnet
-export DOTNET_ROOT=$HOME/dotnet
-export PATH=$PATH:$HOME/dotnet
-dotnet --info
+sudo mkdir -p $HOME/dotnet && tar zxf aspnetcore-runtime-9.0.6-linux-x64.tar.gz -C $HOME/dotnet
+sudo export DOTNET_ROOT=$HOME/dotnet
+sudo export PATH=$PATH:$HOME/dotnet
+sudo dotnet --info
 
-echo 'export DOTNET_ROOT=$HOME/dotnet' >> .bash_profile
-echo 'export PATH=$PATH:$HOME/dotnet' >> .bash_profile
+sudo echo 'export DOTNET_ROOT=$HOME/dotnet' >> .bash_profile
+sudo echo 'export PATH=$PATH:$HOME/dotnet' >> .bash_profile
 
 sudo dnf install -y libicu
 
