81da8197e973f55ace0ee9f52729d325ba0578a3
Aleksandar <acile067@gmail.com>
2025-07-02 10:46:44 +0200

    fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,17 +1,17 @@
-resource "aws_lambda_function" "match_storage" {
+resource aws_lambda_function match_storage {
   function_name = "MatchStorageFunction"
   handler       = "MatchStorage::MatchStorage.Function::FunctionHandler"
   runtime       = "dotnet8"
   role          = aws_iam_role.lambda_exec_role.arn
   timeout       = 10
   memory_size   = 256
-
-  filename         = "${path.module}/lambda/bin/Release/net8.0/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda/bin/Release/net8.0/lambda.zip")
-
+  publish = false
   environment {
     variables = {
       BUCKET_NAME = aws_s3_bucket.match_storage.bucket
     }
+  }
+  lifecycle {
+    ignore_changes = [filename, source_code_hash]
   }
 }
